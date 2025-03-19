10d7a3b980d1df90d8d5e00a0bffa6355ac01b53
Jithin <jithinpulikkal875@gmail.com>
2025-03-19 21:29:08 +0400

    signup completed

---
+++ backend/src/lib/utils.js
@@ -0,0 +1,14 @@
+import jwt from "jsonwebtoken"
+
+export const generateToken = (userId, res)=>{
+    const token = jwt.sign({userId}, process.env.JWT_SECRET, {
+        expiresIn:"5d"
+    })
+    res.cookie("jwt",token,{
+        httpOnly:true, // prevent cross-site scripting attacks
+        maxAge:5*24*60*60*1000,
+        sameSite:"strict", // cross site request forgery attacks
+        secrue: process.env.NODE_ENV != "development"
+    })
+    return token;
+}
