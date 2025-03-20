cab9eed38f2534102ea666016b7d691f219bc31e
Jithin <jithinpulikkal875@gmail.com>
2025-03-20 12:21:42 +0400

    message functionality

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,21 +1,24 @@
-import express from "express"
-import dotenv from "dotenv"
-import cookieParser from "cookie-parser"
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
-import {connectDB} from './lib/db.js'
-import authRoutes from "./routes/auth.routes.js"
+import { connectDB } from "./lib/db.js";
 
-dotenv.config()
-const app = express()
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.route.js";
 
-const PORT = process.env.PORT
+dotenv.config();
+const app = express();
 
-app.use(express.json())
-app.use(cookieParser())
+const PORT = process.env.PORT;
 
-app.use("/api/auth", authRoutes)
+app.use(express.json());
+app.use(cookieParser());
 
-app.listen(PORT, ()=>{
-    console.log("Server is running on port: " + PORT )
-    connectDB()
-})
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
+
+app.listen(PORT, () => {
+    console.log("Server is running on port: " + PORT);
+    connectDB();
+});
