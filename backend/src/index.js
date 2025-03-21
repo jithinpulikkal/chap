afc48643298c7525f2308dc7f3e2ed9612f0422e
Jithin <jithinpulikkal875@gmail.com>
2025-03-21 12:02:25 +0400

    setting themes

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT;
-
+ 
 app.use(express.json());
 app.use(cookieParser());
 app.use(
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,10 +9,14 @@ import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
 import SettingsPage from "./pages/SettingsPage";
 import ProfilePage from "./pages/ProfilePage";
+
 import { useAuthStore } from "./store/useAuthStore";
+import { useThemeStore } from "./store/useThemeStore";
 
 const App = () => {
     const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+
+    const { theme } = useThemeStore();
 
     useEffect(() => {
         checkAuth();
@@ -28,7 +32,7 @@ const App = () => {
         );
 
     return (
-        <div>
+        <div data-theme={theme}>
             <Navbar />
 
             <Routes>
