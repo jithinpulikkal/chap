7e5f518b950457822c021d8ca9f4763f23c7173a
Jithin <jithinpulikkal875@gmail.com>
2025-03-22 18:44:45 +0400

    chat app

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
+import path from 'path'
+
 import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.routes.js";
@@ -15,6 +17,8 @@ dotenv.config();
 
 const PORT = process.env.PORT;
 
+const __dirname = path.resolve()
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(
@@ -26,6 +30,14 @@ app.use(
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+    app.get("*", (req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+    })
+}
 
 // app.listen(PORT, () => {
 //     console.log("Server is running on port: " + PORT);
